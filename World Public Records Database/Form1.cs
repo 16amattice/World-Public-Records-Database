9a70dc2ce4e532f33063a95c0c5dff8dc9c3db39
16amattice <anthonymattice@gmail.com>
2020-06-10 05:17:37 -0500

    Main Code Update

--- World Public Records Database/Form1.cs
+++ World Public Records Database/Form1.cs
@@ -16,5 +16,10 @@ namespace World_Public_Records_Database
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
